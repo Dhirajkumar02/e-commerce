b9746eefdedbd81b299fb37ef31092f09ed4dad2
Dhiraj Kumar <dhirajkumar62413@gmail.com>
2025-04-27 18:15:39 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Footer from './customer/components/Footer/Footer';
 import Navigation from './customer/components/Navigation/Navigation';
-import Product from './customer/components/Product/Product';
+import ProductDetails from './customer/components/ProductDetails/ProductDetails';
 
 
 function App() {
@@ -11,7 +11,8 @@ function App() {
 
       <div>
         {/* <HomePage /> */}
-        <Product />
+        {/* <Product /> */}
+        <ProductDetails />
       </div>
       <Footer />
     </div>
